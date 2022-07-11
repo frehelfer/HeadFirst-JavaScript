68268986a150f0cd0623a6d05efa0ad4fae6a66d
frehelfer <104015488+frehelfer@users.noreply.github.com>
2022-07-11 20:39:19 -0300

    ch7

---
+++ ch7_Methods/duck.js
@@ -0,0 +1,12 @@
+
+function Duck(sound) {
+  this.sound = sound;
+  this.quack = function () {
+    console.log(this.sound);
+  }
+};
+
+let toy = new Duck("quack quack");
+
+console.log(typeof toy);
+console.log(toy instanceof Duck);
